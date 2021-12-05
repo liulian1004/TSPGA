dc34ea450f4de61056a10ce535f77def92b5ff6d
Lian <liulian1004@hotmail.com>
2021-12-04 21:43:56 -0800

    add crossover mutation

--- GA.py
+++ GA.py
@@ -9,6 +9,7 @@ import random
 
 class GA(object):
     population = [];
+    counts = 0;
     
     def __init__(self, counts, sample):
         self.counts = counts;
@@ -35,8 +36,54 @@ class GA(object):
                 curPop.append(number);
             curPop.append(0);
             self.population.append(curPop);
-            
         
         #print(self.population)
         
+    """  
+    generate the index random, and copy the parent2[index, parent2.length-1] as the part of child
+    ex: 
+    index = 2;
+    p1 = [0,1,2,3,0]; 
+    p2 = [0,2,3,1,0]; 
+    child = [0,3,1,2,0]
+    """
+    def crossOver(self, parent1, parent2):
+        # the index shall start from 1, and exclude the end and start index, which represent start city
+        index = random.randint(1, self.counts-2);
+        # this is the part which shall copy from parent2
+        copyPart = parent2[index: len(parent2)-1];
+        child = [];
+        #child start with start city
+        child.append(0);
+        # P1 is the point pointer to the 1 index of parent1
+        p1 = 1;
+        for city in parent1:
+            if city == 0:
+                continue;
+            if p1 == index:
+                child.extend(copyPart)
+            if city not in copyPart:
+                child.append(city)
+            p1 += 1;
+        # child shall end at start city
+        child.append(0);
+        return child;
+    
+    
+    def mutation(self, child):
+         index1 = random.randint(1, len(child)-2);
+         index2 = random.randint(1, len(child)-2);
+         child[index1], child[index2] = child[index2],child[index1];
+         return child;
+                
+def main():
+    ga = GA(4,3);
+    p1 = [0,1,2,3,0];
+    p2 = [0,2,3,1,0];
+    print(GA.crossOver(ga,p1, p2))
+    print(GA.mutation(ga, p1))
+
+if  __name__ == '__main__':
+    main()
+
         
